8257e6fcfa1dc333d7050ee502af53834d69c3b5
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-08-02 12:54:51 +0100

    Set STATIC_ROOT

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
 
 # Default primary key field type
--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path("", include(("app.main.urls", "main"), namespace="main")),
     path("healthcheck/", include("app.healthcheck.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
