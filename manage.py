ef4aa13c128063981c3636c99b716e62ed3fe766
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-12-10 13:37:52 +0000

    Update manage.py to remove test config override

--- manage.py
+++ manage.py
@@ -6,14 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        (
-            "config.settings.test"
-            if sys.argv[1] == "test"
-            else "config.settings.production"
-        ),
-    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
