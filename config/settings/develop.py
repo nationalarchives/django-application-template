6df0f2e839a3cd52b4b249617fc4457f9c223735
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-02-19 14:07:50 +0000

    Check command-line arguments used to start Python process and set IS_TESTING to True if "test" or "pytest" are in the arguments. Works for manage.py test but not python -m pytest

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from config.util import strtobool
 
@@ -9,7 +10,11 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 DEBUG: bool = strtobool(os.getenv("DEBUG", "False"))
 
-if DEBUG:
+IS_TESTING = any(
+    arg in {"test", "pytest"} for arg in sys.argv
+)  # Bypass debug toolbar when running tests, DJDT can't be used with tests (debug_toolbar.E001)
+
+if DEBUG and not IS_TESTING:
     try:
         import debug_toolbar  # noqa: F401
 
