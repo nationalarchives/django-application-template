cd9a735e4b0dd0eb1f4d2223d996685083a01697
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-03-06 10:41:40 +0000

    Remove validation for SECRET_KEY environment variable in settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,6 @@ import os
 from sysconfig import get_path
 
 from config.util import strtobool
-from django.core.exceptions import ImproperlyConfigured
 from django.utils.csp import CSP
 
 from .features import *  # noqa: F401, F403
@@ -147,10 +146,6 @@ except FileNotFoundError:
     pass
 
 SECRET_KEY: str = os.environ.get("SECRET_KEY", "")
-if not SECRET_KEY:
-    raise ImproperlyConfigured(
-        "SECRET_KEY environment variable must be set and cannot be empty."
-    )
 
 DEBUG: bool = False
 
