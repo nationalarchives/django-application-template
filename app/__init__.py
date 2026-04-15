eca8c641577cca723a9153419134f6604eef43db
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-04-15 17:10:15 +0100

    Remove logging statements from __init__.py and update docker-compose.yml to remove LOG_LEVEL environment variable

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,3 @@ from django.conf import settings
 logging.basicConfig(level=settings.LOG_LEVEL)
 
 logger = logging.getLogger(__name__)
-logger.debug("DEBUG")
-logger.info("INFO")
-logger.warning("WARNING")
-logger.error("ERROR")
-logger.critical("CRITICAL")
